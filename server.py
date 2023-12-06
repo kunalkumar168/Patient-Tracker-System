3cc9decdab01fd68e4df739f3d719d6c1e61257c
Kunal Kumar <kunal.kumar.eee14@itbhu.ac.in>
2023-12-05 21:39:48 -0500

    Updated main db and flask server for all functions

--- server.py
+++ server.py
@@ -1,197 +1,277 @@
 from flask import *
 import bcrypt
 import sqlite3
-import json
 import os
-import models.doctor as doctor
 from models.patient import Patient
-import models.medicine as medicine
+from models.medicine import Medicine
 from models.doctor import Doctor
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from models.appointments import Appointment
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-# ---------------------------------- Login Section ------------------------------------------
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///healthdb.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
+
+# Intialise the database
+with app.app_context():
+    db.init_app(app)
+    Patient().setup_db()
+    Appointment().setup_db()
+    Doctor().setup_db()
+    Medicine().setup_db()
+
+# ---------------------------------- Common Section ------------------------------------------
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
-@app.route('/login-patient', methods=['GET'])
-def login_pat():
-    return render_template('login_pat.html')
+def find_user_type(email):
+    conn = sqlite3.connect('./healthdb.db')
+    cur = conn.cursor()
+    cur.execute('SELECT pass FROM patients WHERE email=(?)', (email,))
+    hash_check = cur.fetchone()
+    if hash_check == None:
+        cur.execute('SELECT pass FROM doctors WHERE email=(?)', (email,))
+        hash_check = cur.fetchone()
+        if hash_check == None:
+            return None
+        else:
+            return "Doctor"
+    else:
+        return "Patient"
 
-@app.route('/login-patient', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    user_data = request.get_json()
-    email = user_data['email']
-    password = user_data['password']
-    login = Patient().login(email, password.encode('utf-8'))
+    email = request.form['email']
+    password = request.form['password']
+    user_type = find_user_type(email)
+    print(email, password, user_type)
+    if user_type == "Patient":
+        login = Patient().login(email, password.encode('utf-8'))
+    elif user_type == "Doctor":
+        login = Doctor().login(email, password.encode('utf-8'))
+    else:
+        return render_template('login.html')
+    
+    error = None
     if(login == None):
-        return "No user exists"
+        error = "No user exists"
     elif(login == "WrongPass"):
-        return "Wrong password"
+        error = 'Invalid Credentials. Please try again.'
     else:
         session['auth'] = email
-        return redirect(url_for('afterpatlogin'))
-
-@app.route('/login-doctor', methods=['GET'])
-def login_doc():
-    return render_template('login_doc.html')
-
-@app.route('/login-doctor', methods=['POST'])
-def logindoctor():
-    user_data = request.get_json()
-    email = user_data['email']
-    password = user_data['password']
-    login = Doctor().login(email, password.encode('utf-8'))
-    if (login == None):
-        return "No user exists"
-    elif (login == "WrongPass"):
-        return "Wrong password"
-    else:
-        session['auth'] = email
-        return redirect(url_for('afterdoclogin'))
-
-
-@app.route('/create-patient', methods=['GET'])
-def create_patient_form():
-    return render_template('create-patient.html')
-
-@app.route('/create-patient', methods=['POST'])
-def createpatientfront():
-    user_data = request.get_json()
-    name = user_data['name']
-    email = user_data['email']
-    password = user_data['password']
-    weight = user_data['weight']
-    height = user_data['height']
-    gender = user_data['gender']
-    age = user_data['age']
-    medical_history = user_data['medical_history']
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    #name, email, hash, weight, height, age, gender, medications, medical_history, doc_email
-    try:
-        Patient().create(name, email, hashed_password, weight, height, age, gender, medical_history)
-        return "safe"
-    except sqlite3.Error as error:
-        if 'UNIQUE constraint failed' in str(error):
-            return "User exists"
+        session['user_type'] = user_type
+        if user_type=='Patient':
+            print(session)
+            return redirect(url_for('patient_dashboard'))
         else:
-            return "User with email " + email + " created"
-        
+            return redirect(url_for('doctor_dashboard'))
     
-@app.route('/create-doctor', methods=['GET'])
-def create_doctor_form():
-    return render_template('create-doctor.html')
+    return render_template('login.html', error=error)
 
-@app.route('/create-doctor', methods=['POST'])
-def createdocfront():
-    doc_data = request.get_json()
-    email = doc_data['email']
-    password = doc_data['password']
-    hashed_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    name = doc_data['name']
-    specialization = doc_data['specialization']
-    experience = doc_data['experience']
-    try:
-        Doctor().create(name, email, hashed_pass, specialization, experience)
-        return "done"
-    except sqlite3.Error as error:
-        if 'UNIQUE constraint failed' in str(error):
-            return "User exists"
-        else:
-            return "User with email " + email + " created"
-    
-# --------------------------------- Patient Components -------------------------------------------
+@app.route('/logout')
+def logout():
+    session.pop('auth', None)
+    session.pop('user_type', None)
+    return redirect(url_for('index'))
 
-@app.route('/patient-info', methods=['GET'])
-def afterpatlogin():
-    if 'auth' in session:
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        role = request.form.get('role')
+        if role == 'patient':
+            return redirect(url_for('register_patient'))
+        elif role == 'doctor':
+            return redirect(url_for('register_doctor'))
+    return render_template('register.html')
+
+# # --------------------------------- Patient Components -------------------------------------------
+
+@app.route('/patient-dashboard')
+def patient_dashboard():
+    if 'auth' in session and session['user_type'] == 'Patient':
         email = session['auth']
         patient_details = Patient().viewprofile(email)
-        return render_template('view_pat_profile.html', patient_details=patient_details)
+        session['first_name'] = patient_details['first_name']
+        appointments = Patient().getallappointments(email)
+        return render_template('dashboard/patient_dashboard.html', patient_details=patient_details, appointments=appointments)
     else:
-        return "User not logged in"
+        return redirect(url_for('login'))
+    
+@app.route('/create-patient', methods=['GET', 'POST'])
+def register_patient():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        weight = request.form['weight']
+        height = request.form['height']
+        gender = request.form['gender']
+        age = request.form['age']
+        medical_history = request.form['medical_history']
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        #name, email, hash, weight, height, age, gender, medications, medical_history, doc_email
+        try:
+            Patient().create(name, email, hashed_password, weight, height, age, gender, medical_history)
+            flash('Patient registered successfully!', 'success')
+            return redirect(url_for('register_patient'))
+        except sqlite3.Error as error:
+            if 'UNIQUE constraint failed' in str(error):
+                flash('User already exists!', 'failure')
+                return redirect(url_for('register_patient'))
+            else:
+                flash(f"User with email {email} created!", 'failure')
+                return redirect(url_for('register_patient'))
+    return render_template('patients/register_patient.html')
 
-@app.route('/get-doctor-list', methods=['POST'])
-def getdocforspecialization():
-    req_data = request.get_json()
-    first_name = req_data['first_name']
-    last_name = req_data['last_name']
-    specialization = req_data['specialization']
-    doctors = Doctor().getdoclist(first_name, last_name, specialization)
-    return viewdocfrompat(json.dumps(doctors))
+@app.route('/book-appointment', methods=['GET', 'POST'])
+def book_appointment():
+    doctors = []
+    if request.method == 'POST':
+        if 'auth' in session:
+            search_doctor = any(i in request.form for i in ['first_name', 'last_name', 'specialization'])
+            if search_doctor:
+                first_name = request.form['first_name']
+                last_name = request.form['last_name']
+                specialization = request.form['specialization']
+                doctors = Doctor().getdoclist(first_name, last_name, specialization)
 
-@app.route('/get-doctor-list', methods=['GET'])
-def viewdocfrompat(doctors):
-    return render_template('doc_list.html', doctors=json.dumps(doctors))
+    return render_template('patients/book_appointment.html', doctors=doctors)
 
-@app.route('/add-doctor', methods=['POST'])
-def adddoctor():
-    if 'auth' not in session:
-        abort(401)
-    else:
-        user_data = request.get_json()
-        doctor_email = user_data['doctor_email']
-        result = Patient.bookdoctor(doctor_email)
-        return result
+@app.route('/book_doctor/<string:doctor_email>/<string:doctor_name>', methods=['GET', 'POST'])
+def book_doctor(doctor_email, doctor_name):
+    if request.method == 'POST':
+        if 'auth' in session:
+            patient_email = session['auth']
+            date = request.form.get('date')
+            time = request.form.get('time')
+            reason = request.form.get('reason')
+            try:
+                Appointment().create(patient_email, doctor_email, date, time, reason)
+                flash('Appointment created successfully!', 'success')
+            except sqlite3.Error as error:
+                if 'UNIQUE constraint failed' in str(error):
+                    flash('Appointment can\'t be booked. Try again!', 'failure')
 
-# @app.route('/patient-dashboard', methods=['GET'])
-# def get_doc_list():
-#     return render_template('view_doc_list.html')
+    return render_template('patients/book_doctor.html', doctor_email=doctor_email, doctor_name=doctor_name)
 
-# --------------------------------- Doctor Components -------------------------------------------
+@app.route('/patient_info/<string:patient_email>', methods=['GET'])
+def patient_info(patient_email):
+    patient_info = Patient().getinfo(patient_email)
+    return render_template('patients/patient_info.html', patient_info=patient_info)
 
-@app.route('/doctor-info', methods=['GET'])
-def afterdoclogin():
+@app.route('/patient_prescription/<string:doctor_email>', methods=['GET'])
+def patient_prescription(doctor_email):
     if 'auth' in session:
+        patient_email = session['auth']
+        patient_info = Patient().getprescription(patient_email, doctor_email)
+        return render_template('patients/patient_prescription.html', patient_info=patient_info)
+    
+@app.route('/cancel_appointment/<string:doctor_email>/<action>', methods=['GET'])
+def cancel_appointment(doctor_email, action):
+    if 'auth' in session:
+        patient_email = session['auth']
+        if action.lower() == 'cancel':
+            Patient().cancelappointment(patient_email, doctor_email)
+
+    return redirect(url_for('patient_dashboard'))
+
+@app.route('/edit_appointment/<string:doctor_email>', methods=['GET', 'POST'])
+def edit_appointment(doctor_email):
+    patient_name = None
+    if request.method == 'POST':
+        if 'auth' in session:
+            patient_email = session['auth']
+            patient_data = Patient().getinfo(patient_email)
+            patient_name = patient_data['name']
+            date = request.form.get('new_date')
+            time = request.form.get('new_time')
+            try:
+                Patient().editappointment(patient_email, doctor_email, date, time)
+                flash('Appointment updated successfully!', 'success')
+            except sqlite3.Error as error:
+                flash('Appointment can\'t be updated. Try again!', 'failue')
+    
+    return render_template('patients/edit_appointment.html', doctor_email=doctor_email, patient_name=patient_name)
+    
+
+# # --------------------------------- Doctor Components -------------------------------------------
+
+
+@app.route('/doctor-dashboard')
+def doctor_dashboard():
+    if 'auth' in session and session['user_type'] == 'Doctor':
         email = session['auth']
-        print(email)
         doctor_details = Doctor().viewprofile(email)
-        return render_template('view_doc_profile.html', doctor_details=doctor_details)
+        session['first_name'] = doctor_details['first_name']
+        appointments = Doctor().getallappointments(email)
+        return render_template('dashboard/doctor_dashboard.html', doctor_details=doctor_details, appointments=appointments)
     else:
-        return "User not logged in"
+        return redirect(url_for('login'))
+    
+@app.route('/create-doctor', methods=['GET', 'POST'])
+def register_doctor():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        specialization = request.form['specialization']
+        experience = request.form['experience']
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        #name, email, hash, specialization, experience
+        try:
+            Doctor().create(name, email, hashed_password, specialization, experience)
+            flash('Doctor registered successfully!', 'success')
+            return redirect(url_for('register_doctor'))
+        except sqlite3.Error as error:
+            if 'UNIQUE constraint failed' in str(error):
+                flash('User already exists!', 'failure')
+                return redirect(url_for('register_doctor'))
+            else:
+                flash(f"User with email {email} created!", 'failure')
+                return redirect(url_for('register_doctor'))
+    return render_template('doctors/register_doctor.html')
 
-@app.route('/edit-patient-doc', methods=['PATCH'])
-def editpatientdoc():
-    if 'auth' not in session:
-        abort(401)
-    else:
-        user_data = request.get_json()
-        doc_email = user_data['email']
-        prescription = user_data['prescription']
-        result = Doctor.editpatient(doc_email, prescription)
-        return result
-        #email, prescription
+@app.route('/doctor_info/<string:doctor_email>', methods=['GET'])
+def doctor_info(doctor_email):
+    doctor_info = Doctor().getinfo(doctor_email)
+    return render_template('doctors/doctor_info.html', doctor_info=doctor_info)
 
-@app.route('/get-patient-data', methods=['GET'])
-def fetch_patient():
-    if 'auth' not in session:
-        abort(401)
-    else:
-        patient_info = Patient.fetchdata()
-        return patient_info
+@app.route('/edit_patient/<string:patient_email>/<string:patient_name>', methods=['GET', 'POST'])
+def edit_patient(patient_email, patient_name):
+    if request.method == 'POST':
+        if 'auth' in session:
+            doctor_email = session['auth']
+            prescription = request.form['prescription']
+            status = request.form['status']
+            try:
+                Doctor().editpatient(patient_email, doctor_email, prescription, status)
+                flash(f"Added prescription successfully!", 'success')
+            except sqlite3.Error as error:
+                flash(f"Edit unsuccessful. Try again!", 'failure')
+    
+    return render_template('doctors/edit_patient_appointment.html', patient_email=patient_email, patient_name=patient_name)
+
+@app.route('/pending_request/<string:patient_email>/<action>', methods=['GET'])
+def pending_request(patient_email, action):
+    if 'auth' in session:
+        doctor_email = session['auth']
+        accept = 'accept' if action.lower()=='accept' else None
+        reject = 'reject' if action.lower()=='reject' else None
+        try:
+            Doctor().pendingrequest(patient_email, doctor_email, accept, reject)
+            flash(f"Performed request successfully!", 'success')
+        except sqlite3.Error as error:
+            flash(f"Action unsuccessful. Try again!", 'failure')
+    
+    return redirect(url_for('doctor_dashboard'))
 
 
-@app.route('/create-medicine', methods=['POST'])
-def createmedicinefront():
-    med_data = request.get_json()
-    #id, name, mfg_date, exp_date, description, patient_email
-    id = med_data['id']
-    name = med_data['name']
-    mfg_date = med_data['mfg_date']
-    exp_date = med_data['exp_date']
-    description = med_data['description']
-    patient_email = med_data['patient_email']
-    try:
-        medicine.create(id, name, mfg_date, exp_date, description, patient_email)
-        return "safe"
-    except sqlite3.Error as error:
-        if 'UNIQUE constraint failed' in str(error):
-            return "Medicine exists"
-        else:
-            return "Medicine " + name + " created"
+# # --------------------------------- Main Function -------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)
