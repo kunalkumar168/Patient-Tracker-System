9dbc147f513ef860c838cc9e330e65aeb2f4b533
Kunal Kumar <kunal.kumar.eee14@itbhu.ac.in>
2023-12-14 23:17:52 -0500

    Added docstring and comments for Understability

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ UPLOAD_FOLDER = './files/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy()
 
-# Intialise the database
+# Initialize the database
 with app.app_context():
     db.init_app(app)
     Patient().setup_db()
@@ -31,9 +31,24 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    """
+    Render the main login page.
+
+    Returns:
+        Flask Response: Rendered template for the login page.
+    """
     return render_template('login.html')
 
 def find_user_type(email):
+    """
+    Find the type of user (Patient or Doctor) based on the email.
+
+    Args:
+        email (str): User's email address.
+
+    Returns:
+        str or None: User type ('Patient' or 'Doctor') if found, None otherwise.
+    """
     conn = sqlite3.connect('./healthdb.db')
     cur = conn.cursor()
     cur.execute('SELECT pass FROM patients WHERE email=(?)', (email,))
@@ -50,6 +65,12 @@ def find_user_type(email):
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    Handle user login.
+
+    Returns:
+        Flask Response: Redirects to the appropriate dashboard based on user type.
+    """
     email = request.form['email']
     password = request.form['password']
     user_type = find_user_type(email)
@@ -77,12 +98,24 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """
+    Handle user logout.
+
+    Returns:
+        Flask Response: Redirects to the main login page.
+    """
     session.pop('auth', None)
     session.pop('user_type', None)
     return redirect(url_for('index'))
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    Render the registration page and handle user registration.
+
+    Returns:
+        Flask Response: Rendered template for registration or redirection to specific registration page.
+    """
     if request.method == 'POST':
         role = request.form.get('role')
         if role == 'patient':
@@ -95,6 +128,12 @@ def register():
 
 @app.route('/patient-dashboard')
 def patient_dashboard():
+    """
+    Render the patient dashboard.
+
+    Returns:
+        Flask Response: Rendered template for the patient dashboard.
+    """
     if 'auth' in session and session['user_type'] == 'Patient':
         email = session['auth']
         patient_details = Patient().viewprofile(email)
@@ -108,6 +147,12 @@ def patient_dashboard():
 
 @app.route('/create-patient', methods=['GET', 'POST'])
 def register_patient():
+    """
+    Render the patient registration page and handle patient registration.
+
+    Returns:
+        Flask Response: Rendered template for patient registration or redirection to registration page.
+    """
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
@@ -134,6 +179,12 @@ def register_patient():
 
 @app.route('/edit_health_records', methods=['GET', 'POST'])
 def edit_health_records():
+    """
+    Render the page for editing patient health records and handle the update.
+
+    Returns:
+        Flask Response: Rendered template for editing health records.
+    """
     if 'auth' in session and session['user_type'] == 'Patient':
         if request.method == 'POST':
             patient_email = session['auth']
@@ -155,6 +206,12 @@ def edit_health_records():
 
 @app.route('/book-appointment', methods=['GET', 'POST'])
 def book_appointment():
+    """
+    Render the page for booking an appointment and handle the appointment creation.
+
+    Returns:
+        Flask Response: Rendered template for booking an appointment.
+    """
     doctors = []
     if request.method == 'POST':
         if 'auth' in session:
@@ -169,6 +226,16 @@ def book_appointment():
 
 @app.route('/book_doctor/<string:doctor_email>/<string:doctor_name>', methods=['GET', 'POST'])
 def book_doctor(doctor_email, doctor_name):
+    """
+    Render the page for booking a specific doctor and handle the appointment creation.
+
+    Args:
+        doctor_email (str): Email of the selected doctor.
+        doctor_name (str): Name of the selected doctor.
+
+    Returns:
+        Flask Response: Rendered template for booking a specific doctor.
+    """
     if request.method == 'POST':
         if 'auth' in session:
             patient_email = session['auth']
@@ -202,6 +269,15 @@ def book_doctor(doctor_email, doctor_name):
 
 @app.route('/patient_prescription/<string:doctor_email>', methods=['GET'])
 def patient_prescription(doctor_email):
+    """
+    Render the page displaying the prescription for a specific patient.
+
+    Args:
+        doctor_email (str): Email of the doctor.
+
+    Returns:
+        Flask Response: Rendered template for displaying patient prescription.
+    """
     if 'auth' in session:
         patient_email = session['auth']
         patient_info = Patient().getprescription(patient_email, doctor_email)
@@ -209,6 +285,16 @@ def patient_prescription(doctor_email):
     
 @app.route('/cancel_appointment/<string:doctor_email>/<action>', methods=['GET'])
 def cancel_appointment(doctor_email, action):
+    """
+    Cancel or reschedule an appointment for a patient with a specific doctor.
+
+    Args:
+        doctor_email (str): Email of the doctor.
+        action (str): Action to perform, either 'cancel' or 'reschedule'.
+
+    Returns:
+        Flask Response: Redirects to the patient dashboard after performing the action.
+    """
     if 'auth' in session:
         patient_email = session['auth']
         if action.lower() == 'cancel':
@@ -218,6 +304,15 @@ def cancel_appointment(doctor_email, action):
 
 @app.route('/edit_appointment/<string:doctor_email>', methods=['GET', 'POST'])
 def edit_appointment(doctor_email):
+    """
+    Render the page for editing an appointment and handle the update.
+
+    Args:
+        doctor_email (str): Email of the selected doctor.
+
+    Returns:
+        Flask Response: Rendered template for editing an appointment.
+    """
     patient_name = None
     if request.method == 'POST':
         if 'auth' in session:
@@ -242,6 +337,12 @@ def edit_appointment(doctor_email):
 
 @app.route('/upload-report', methods=['GET', 'POST'])
 def upload_reports():
+    """
+    Handle the uploading of medical reports by patients.
+
+    Returns:
+        Flask Response: Redirects to the patient dashboard after handling the upload.
+    """
     if request.method == 'POST':
         if 'auth' in session:
             patient_email = session['auth']
@@ -252,9 +353,9 @@ def upload_reports():
 
             try:
                 ReportFile().create(patient_email=patient_email, doctor_email=None, report_name=name, file_path=file_path)
-                flash('Reports was uploaded!', 'success')
+                flash('Reports were uploaded!', 'success')
             except sqlite3.Error as error:
-                flash('Reports was not uploaded. Try again!', 'failue')
+                flash('Reports were not uploaded. Try again!', 'failue')
     else:
         if 'auth' in session:
             patient_email = session['auth']
@@ -265,6 +366,15 @@ def upload_reports():
         
 @app.route('/serve-report/<path:file_path>')
 def serve_report(file_path):
+    """
+    Serve a medical report file.
+
+    Args:
+        file_path (str): Path to the report file.
+
+    Returns:
+        Flask Response: Serves the report file.
+    """
     if 'auth' in session:
         return send_from_directory(app.config['UPLOAD_FOLDER'], os.path.basename(file_path), as_attachment=False)
     
@@ -272,17 +382,35 @@ def serve_report(file_path):
 
 @app.route('/delete-report/<path:report_name>', methods=['POST'])
 def delete_report(report_name):
+    """
+    Delete a medical report for a patient.
+
+    Args:
+        report_name (str): Name of the report to delete.
+
+    Returns:
+        Flask Response: Redirects to the upload reports page after handling the deletion.
+    """
     if 'auth' in session:
         patient_email = session['auth']
         try:
             Patient().deletereport(pat_email=patient_email, report_name=report_name)
         except sqlite3.Error as error:
-            flash('Reports was not uploaded. Try again!', 'failue')
+            flash('Reports were not uploaded. Try again!', 'failue')
     
     return redirect(url_for('upload_reports'))
 
 @app.route('/get_doctor_availability/<string:doctor_email>')
 def get_doctor_availability(doctor_email):
+    """
+    Get the availability of a doctor for a selected date.
+
+    Args:
+        doctor_email (str): Email of the doctor.
+
+    Returns:
+        JSON Response: Availability information in FullCalendar event format.
+    """
     selected_date = request.args.get('selected_date')
 
     # Fetch the availability from the database for the selected date
@@ -302,6 +430,12 @@ def get_doctor_availability(doctor_email):
 
 @app.route('/doctor-dashboard')
 def doctor_dashboard():
+    """
+    Render the doctor's dashboard.
+
+    Returns:
+        Flask Response: Rendered template for the doctor's dashboard.
+    """
     if 'auth' in session and session['user_type'] == 'Doctor':
         email = session['auth']
         doctor_details = Doctor().viewprofile(email)
@@ -313,6 +447,12 @@ def doctor_dashboard():
     
 @app.route('/create-doctor', methods=['GET', 'POST'])
 def register_doctor():
+    """
+    Render the page for doctor registration and handle the doctor registration process.
+
+    Returns:
+        Flask Response: Rendered template for doctor registration or redirects to registration completion.
+    """
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
@@ -336,13 +476,31 @@ def register_doctor():
 
 @app.route('/doctor_info/<string:doctor_email>', methods=['GET'])
 def doctor_info(doctor_email):
+    """
+    Render the page displaying information about a specific doctor.
+
+    Args:
+        doctor_email (str): Email of the doctor.
+
+    Returns:
+        Flask Response: Rendered template for displaying doctor information.
+    """
     doctor_info = Doctor().getinfo(doctor_email)
     return render_template('doctors/doctor_info.html', doctor_info=doctor_info)
 
 
 @app.route('/patient_info/<string:patient_email>', methods=['GET'])
 def patient_info(patient_email):
-     if 'auth' in session:
+    """
+    Render the page displaying information about a specific patient.
+
+    Args:
+        patient_email (str): Email of the patient.
+
+    Returns:
+        Flask Response: Rendered template for displaying patient information.
+    """
+    if 'auth' in session:
         doctor_email = session['auth']
         patient_info = Patient().getinfo(patient_email)
         patient_reports = Doctor().viewpatientreports(pat_email=patient_email, doc_email=doctor_email)
@@ -350,6 +508,16 @@ def patient_info(patient_email):
 
 @app.route('/edit_patient/<string:patient_email>/<string:patient_name>', methods=['GET', 'POST'])
 def edit_patient(patient_email, patient_name):
+    """
+    Render the page for editing patient information and handle the update.
+
+    Args:
+        patient_email (str): Email of the patient.
+        patient_name (str): Name of the patient.
+
+    Returns:
+        Flask Response: Rendered template for editing patient information.
+    """
     if request.method == 'POST':
         if 'auth' in session:
             doctor_email = session['auth']
@@ -365,6 +533,16 @@ def edit_patient(patient_email, patient_name):
 
 @app.route('/pending_request/<string:patient_email>/<action>', methods=['GET'])
 def pending_request(patient_email, action):
+    """
+    Handle pending requests for a patient.
+
+    Args:
+        patient_email (str): Email of the patient.
+        action (str): Action to perform, either 'accept' or 'reject'.
+
+    Returns:
+        Flask Response: Redirects to the doctor's dashboard after handling the request.
+    """
     if 'auth' in session:
         doctor_email = session['auth']
         accept = 'accept' if action.lower()=='accept' else None
@@ -379,6 +557,12 @@ def pending_request(patient_email, action):
 
 @app.route('/set_availability', methods=['GET', 'POST'])
 def set_availability():
+    """
+    Render the page for setting the availability of a doctor and handle the availability update.
+
+    Returns:
+        Flask Response: Rendered template for setting doctor availability or redirects to availability completion.
+    """
     if request.method == 'POST':
         email = session['auth']
         date = request.form['date']
